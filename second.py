7289bc650842cc7ab12ccd255c803898ef802889
Naveen <naveenchandra.nc10@gmail.com>
2024-06-26 13:37:48 +0530

    added another number

--- second.py
+++ second.py
@@ -1,8 +1,14 @@
 a = 10
+b = 25
 if a % 2 == 0:
     print(a "is even number")
 else:
     print(a "is odd number")
+if b % 2 == 0:
+    print(b "is even number")
+else:
+    print(b "is odd number")
+
     
     
 
